024a2f6da455a6687b4c428d98df08043c604aff
Marcelomoraiscurto <marcelo.curto@aluno.faculdadeimpacta.com.br>
2021-08-22 00:50:57 -0300

    Convertendo credential em variável

--- providers.tf
+++ providers.tf
@@ -1,5 +1,4 @@
-terraform 
-{
+terraform {
   required_providers {
     google = {
       source = "hashicorp/google"
@@ -11,10 +10,7 @@ terraform
 # Configura o Provider Google Cloud com o Projeto
 provider "google" {
 
-  
   project = "lab-devops-cloud-marcelomoraiz"
   region  = "us-central1"
   zone    = "us-central1-c"
-
-
 }
